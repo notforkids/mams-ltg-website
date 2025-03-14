55360dabcec495facbc4572b57c8bcad6f8f41f6
notforkids <kimjohnsario.atento@bicol-u.edu.ph>
2025-03-14 14:04:32 +0800

    Updated 03142025 204

--- src/components/HomeComponent/StatsSection.jsx
+++ src/components/HomeComponent/StatsSection.jsx
@@ -3,17 +3,18 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import StatsPic from '../../img/stats-pic.png';
 
 
 const StatsSection = () => {
   return (
     <Container fluid className='bg-light custom-x-padding py-5'>
-    <Row className='justify-content-center'>
-        <Col md={6} sm={12} className='mt-5'>
-        pic
+    <Row className='d-flex flex-column-reverse flex-md-row align-items-center '>
+        <Col md={6} sm={12} className='mt-5 d-flex justify-content-center align-items-center'>
+        <img src={StatsPic} className='img-fluid rounded' alt='stats pic' />
         </Col>
         
-        <Col md={6} sm={12} className='mt-md-5 '>
+        <Col md={6} sm={12} className='mt-md-5 mt-sm-5'>
         <h6 className='fw-bold'>Success</h6>
         <h1 className="display-4 fw-bold mt-3">Transforming Lives Through Microfinance Solutions</h1>
         <p className='lead mt-3'>LT&G Credit Line has empowered thousands of entrepreneurs across the Philippines. Our innovative microlending approach has made financial access a reality for many. </p>
