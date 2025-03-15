e7cb1d69763179ff635684a4b6d2fef6ebda4d83
notforkids <kimjohnsario.atento@bicol-u.edu.ph>
2025-03-15 09:33:44 +0800

    Updated 13152025

--- src/components/HomeComponent/TestimonialSection.jsx
+++ src/components/HomeComponent/TestimonialSection.jsx
@@ -2,13 +2,35 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import FranchiseeLogo from '../../img/samplelogo.png';
+import Franchisee from '../../img/paolo.png';
 
 const TestimonialSection = () => {
   return (
     <Container fluid className='bg-dark custom-x-padding py-5'>
-        <Row className='text-center'>
-            <Col>
-                <h1 className='text-light'>Testimonial Section</h1>
+        <Row className='text-center justify-content-center'>
+            <Col lg={8} className='mt-5'>
+                <img 
+                src={FranchiseeLogo} 
+                 width="auto"
+                 height="45"
+                alt='Franchisee Logo' 
+                className='' />
+                <h4 className='text-light mt-5'> "The system is automated, user-friendly and it is also easy to be monitored, 
+                which made it very convenient to the staffs. What makes a franchise impressive is 
+                that they are helping other people to enrich their lives. 
+                The head office as well as the area supervisor are present to give their support 
+                especially now that we are currently experiencing a pandemic, they give advises 
+                and tips that helped us in this kind of situation. I am more than happy to be a 
+                franchisee owner of LT&G, I can say that you’re safe in hands."</h4>
+                <img 
+                src={Franchisee} 
+                 width="auto"
+                 height="80"
+                alt='Franchisee Pic' 
+                className='mt-5' />
+                <h4 className='text-light fw-bold mt-3'>Paolo S. Bobier</h4>
+                <h7 className='text-light'>Franchise Owner, One SM Lending Corporation</h7>
             </Col>
         </Row>
     </Container>
